d1a466e16eb0c08706e47c31aad65df02489e964
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-17 15:12:33 +0530

    added symbols image,updated main_menu,partially conceptualized
    i am trying to make this a 7 gestures game in which any 2 pair have 1 weakness , a single element has 3 weakness. i have added timer and a label that indicates which player turn it is and also added the template for the function match i have partially conceptualized who wins against who after i finally finalized the rules i will replace the value in match with actual values. added 7 buttons out of which only 2 can be selected

--- rps-extreme/main_game.gd
+++ rps-extreme/main_game.gd
@@ -1,3 +1,78 @@
-extends Node2D
+extends Control
+var element_selected = 0
 func _ready():
-    print("ready")
+	$Label.text = "Player1 turn"
+func _process(_delta: float) -> void:
+	$Time_left.text = "Timer: " + str($Timer.time_left).substr(0,str($Timer.time_left).find(".")+2)
+	if(element_selected>1):
+		$Timer.stop()
+		$Timer.timeout.emit()
+		element_selected=0
+
+func _on_timer_timeout() -> void:
+	if($Label.text == "Player1 turn"):
+		$Label.text = "Player2 turn"
+	else:
+		$Label.text = "Player1 turn"
+	$Timer.start()
+#details
+#we have rock paper scirssors fire water air laser
+#rock crushes scirssors,put out fire,blocks laser rock wins
+#rock covered by paper,drown in water,carried by air rock lose
+#scissors cuts paper,reflect laser,cuts throw air scirssors win
+#scirssors get crushed by rock,melts by fire,drown in water sccissors lose
+#laser cuts air, evaporates water,passes through fire laser wins
+#laser get reflected by scissors,blocked by rock,deflected by paper
+#air shifts rock,extinguish fire,carries paper 
+#air get bush bush by scissors,cutted by laser,dissolved in water
+#fire burns paper,
+#fire get extinguish by air,
+#paper covers rock,becomes boat and float in water,
+#paper get decayed by laser,cuts by scissors,get burned by fire
+#water
+
+#confirm things:
+#rock defeated scissors
+#scissores defeat paper
+#paper defeats rock
+#air defeates fire
+#fire defeates laser,scissors,paper
+#fire get defeated by rock,water,air
+#water defeates fire
+#laser get defeated by scissos,rock,paper
+
+#order
+#fire
+
+#laser
+#
+#
+#
+#
+
+
+func _on_paper_pressed() -> void:
+	element_selected+=1
+
+
+func _on_scissors_pressed() -> void:
+	element_selected+=1
+
+
+func _on_rock_pressed() -> void:
+	element_selected+=1
+
+
+func _on_fire_pressed() -> void:
+	element_selected+=1
+
+
+func _on_laser_pressed() -> void:
+	element_selected+=1
+
+
+func _on_water_pressed() -> void:
+	element_selected+=1
+
+func _on_air_pressed() -> void:
+	element_selected+=1
