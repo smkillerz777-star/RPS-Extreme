e1b0f39ceabd03aff6fd5a0806438997b6e69d30
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-18 02:14:59 +0530

    completed instruction page
    added a graph representing the rules, also made the forward and backward key functionable and divided instruction into 2 pages that can be navigated by using forward and backward button

--- rps-extreme/instructions.gd
+++ rps-extreme/instructions.gd
@@ -1,1 +1,21 @@
 extends Control
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+
+func _on_backward_pressed() -> void:
+	$forward.visible = true
+	if($page2.visible == true):
+		$page1.visible = true
+		$page2.visible = false
+		$backward.visible = false
+
+func _on_forward_pressed() -> void:
+	$backward.visible = true
+	if($page1.visible == true):
+		$page1.visible = false
+		$page2.visible = true
+		$forward.visible = false
