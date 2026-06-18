7d1b92a154fadcffd66c1450272a5c6b3824aa67
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-19 00:22:24 +0530

    added settings page, cards picture in main menu
    add the settings page where the players can select how many rounds they want to play , and how many cards they want to select at each round

--- rps-extreme/main_menu.gd
+++ rps-extreme/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 func _ready() -> void:
 	$Play_Button.add_theme_font_size_override("font_size",global.font_size)
 	$Instructions_Button.add_theme_font_size_override("font_size",global.font_size)
+	$Setting_Button.add_theme_font_size_override("font_size",global.font_size)
 	$Title.add_theme_font_size_override("font_size",global.heading_font_size)
 
 func _on_button_pressed() -> void:
@@ -10,3 +11,7 @@ func _on_button_pressed() -> void:
 
 func _on_instructions_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://instructions.tscn")
+
+
+func _on_setting_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings.tscn")
